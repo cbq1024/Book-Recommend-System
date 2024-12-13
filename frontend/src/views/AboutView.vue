80e62acd318792e1348e8cba7d43259662db02a7
caobei <caobei1024@gmail.com>
2024-12-13 21:48:15 +0800

    :sparkles: feat(core): init backend,frontend
    - init sapring security(login,register)
    - init db with docker
    - init map struct
    - init vue
    - init tailwindcss

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<h1 class="font-bold text-4xl bg-sky-300">About View</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<h1 class="font-bold text-4xl bg-pink-600">Home View</h1>
+</template>
+
+<style scoped>
+
+</style>
